5826ba2b6114004be37bf6e2fbf140a007843405
tiantan <z5133160@gmail.com>
2020-05-19 16:55:22 +0800

    updating our application with reselect, replaced cart selector with meoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {toggleCartHidden} from '../../redux/cart/cart.actions'
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors'
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss'
 
 //second step is then adding {toggleCartHidden} for updating purpose
 // third step is adding cartreducer to root reducer
-const CartIcon=({toggleCartHidden})=>(
+const CartIcon=({toggleCartHidden,itemCount})=>(
 
 <div className='cart-icon' onClick={toggleCartHidden}> 
 <ShoppingIcon className='shopping-icon'/>
-<span className='item-count'>0</span>
+<span className='item-count'>{itemCount}</span>
 
 </div>
 )
 
-const mapDispatchToProps=dispatch =>({
+const mapDispatchToProps=dispatch =>({ 
 toggleCartHidden:()=>dispatch(toggleCartHidden())
 });     //first step is to write this function 
 
+const mapStateToProps= (state)=>({   //117
+itemCount: selectCartItemsCount(state)//119, notice how the 'state' flows in cart.selector 
+}) //this part code is called selector, because we only use small portion
+// of the state, and every time the state changes, this part will be re-render 
+//even nothing was changed in this part, so what we want is that  if nothing gets
+//changed, we don't re-render this component, this is called memorization,
+//which is the caching of the selectors value and we can achieve this memorization
+//by using a library called Re-select 
 
 
-export default connect(null,mapDispatchToProps)(CartIcon)
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon)
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,25 @@
+import {createSelector} from 'reselect';
+
+const selectCart=state=>state.cart;// to invoke following two functions,
+//all function parameters is originated from here, e.g, 
+//selectCartItemsCount(state) in cart-icon.component.jsx, the state
+//is originated from here
+
+
+export const selectCartItems=createSelector(
+[selectCart],
+cart=>cart.cartItems
+
+)
+
+export const selectCartItemsCount=createSelector(
+    [selectCartItems],
+    cartItems=>
+    cartItems.reduce((accumalatedQuantity,cartItem)=>accumalatedQuantity+cartItem.quantity,
+0)
+)
+
+//2 arguments needs to be passed, the first one is 
+//a collection so an array of input selectors
+//the second argument is a function in which it will 
+//return the value we want out of the selector
